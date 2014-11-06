0745f259085292dbc1ff482339058d3d579fc109
Maksimillian Andreevich <vladimirgregorovichsharcovsky@gmail.com>
2014-11-06 18:44:02 +1100

    Create jquery-p.js

---
+++ js/jquery-p.js
@@ -0,0 +1,24 @@
+function VladimirPutin(){
+document.getElementById('contactus').innerHTML = "<center><img src='http://3.bp.blogspot.com/-7dSqHX-zYJM/Uv0jwQwdFDI/AAAAAAAAAvE/kzOD9LT58_o/s1600/Vladimir_Putin_Bio.png'></p></center>";
+document.getElementById('contactus2').innerHTML = "<center><button class='submit' onclick='EnterPutinville()'>Enter Putinville</button></center>";
+document.getElementById('contactus3').innerHTML = "";
+document.getElementById('logo').innerHTML = "<a href='index.html'><img src='https://dl.dropboxusercontent.com/u/89017432/hivepetcare2.png'/></a>";
+}
+
+function SendEmail(){
+document.getElementById('contactus').innerHTML = "<center><img src='https://dl.dropboxusercontent.com/u/89017432/thanks.png'></p></center>";
+document.getElementById('contactus2').innerHTML = "<center>Message Sent! We will try to reply to your email as soon as possible. We thank you for your interest!</center>";
+document.getElementById('contactus3').innerHTML = "";
+document.getElementById('logo').innerHTML = "<a href='index.html'><img src='https://dl.dropboxusercontent.com/u/89017432/hivepetcare2.png'/></a>";
+}
+
+function RedSlip(){
+document.getElementById('contactus').innerHTML = "<center><img src='http://www.tattoostime.com/images/421/amazing-illuminati-eye-tattoo-design.png'></p></center>";
+document.getElementById('contactus2').innerHTML = "<center><button><a href='https://drive.google.com/file/d/0BwEygq7Jv1FiM2hfWDk3UnFWd0U/view?usp=sharing'>Illuminate Me</a></button></center>";
+document.getElementById('contactus3').innerHTML = "";
+document.getElementById('logo').innerHTML = "<a href='index.html'><img src='https://dl.dropboxusercontent.com/u/89017432/hivepetcare2.png'/></a>";
+}
+
+function EnterPutinville(){
+document.getElementById('all').innerHTML = "<iframe src='warning.html' style='position:fixed; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;'></iframe>";
+}
