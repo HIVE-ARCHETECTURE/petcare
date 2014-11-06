f13e70ccfaba110dfd5b1a258284b37b6665ea9f
Maksimillian Andreevich <vladimirgregorovichsharcovsky@gmail.com>
2014-11-06 20:55:44 +1100

    Create tipoftheday.js

---
+++ js/tipoftheday.js
@@ -0,0 +1,11 @@
+var r_tip = new Array ();
+var i = Math.floor(9*Math.random())
+r_tip[0] = "Not to be confused with Aperture Pet Care";
+r_tip[1] = "Not to be confused with Aperture Pet Care"; //Intentional Duplicate
+r_tip[2] = "Do not name your dog GLaDOS";
+r_tip[3] = "Avoid spraying deadly neurotoxins on your gerbil";
+r_tip[4] = "DO NOT GIVE A HAMSTER A PORTAL GUN!";
+r_tip[5] = "Refrain from sending a dog named Wheatley to the moon";
+r_tip[6] = "We have better customer service than Black Mesa!";
+r_tip[7] = "Consider getting a Companion Cube as your next pet";
+r_tip[8] = "Try an Aperture Science Counterproductive Canine Distribution Device";
