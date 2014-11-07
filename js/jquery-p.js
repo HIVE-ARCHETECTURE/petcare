5dbe8cf4be53fc7a44defaf711855c1e85d909d6
Maksimillian Andreevich <vladimirgregorovichsharcovsky@gmail.com>
2014-11-07 11:53:09 +1100

    Update jquery-p.js

--- js/jquery-p.js
+++ js/jquery-p.js
@@ -5,6 +5,13 @@ document.getElementById('contactus3').innerHTML = "";
 document.getElementById('logo').innerHTML = "<a href='index.html'><img src='https://dl.dropboxusercontent.com/u/89017432/hivepetcare2.png'/></a>";
 }
 
+function BobMarley(){
+document.getElementById('contactus').innerHTML = "<center><img src='http://lh5.ggpht.com/-VDfSbPYsJsg/UQlLdC9WqcI/AAAAAAAAOMY/hzgzDO5DWX4/s9000/Render%2B-%2BBob%2BMarley%2BBaixe%2BRenders.png'></p></center>";
+document.getElementById('contactus2').innerHTML = "<center><button class='submit' onclick='EnterMarleyville()'>Enter Marleyville</button></center>";
+document.getElementById('contactus3').innerHTML = "";
+document.getElementById('logo').innerHTML = "<a href='index.html'><img src='https://dl.dropboxusercontent.com/u/89017432/hivepetcare2.png'/></a>";
+}
+
 function SendEmail(){
 document.getElementById('contactus').innerHTML = "<center><img src='https://dl.dropboxusercontent.com/u/89017432/thanks.png'></p></center>";
 document.getElementById('contactus2').innerHTML = "<center>Message Sent! We will try to reply to your email as soon as possible. We thank you for your interest!</center>";
@@ -21,6 +28,10 @@ document.getElementById('logo').innerHTML = "<a href='index.html'><img src='http
 
 function EnterPutinville(){
 document.getElementById('all').innerHTML = "<iframe src='warning.html' style='position:fixed; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;'></iframe>";
+}
+
+function EnterMarleyville(){
+document.getElementById('all').innerHTML = "<iframe src='marley.html' style='position:fixed; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;'></iframe>";
 }
 var r_tip = new Array ();
 var i = Math.floor(9*Math.random())
